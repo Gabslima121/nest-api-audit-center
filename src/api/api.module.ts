b5d770d30a58655a633732ffd3314947f2b7a02e
Gabslima121 <glima@vixting.com.br>
2022-06-30 22:04:40 -0300

    db: added company table

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -4,8 +4,9 @@ import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { UserRoleModule } from './user-role/user-role.module';
 import { AuthModule } from './auth/auth.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [UserModule, RoleModule, UserRoleModule, AuthModule],
+  imports: [UserModule, RoleModule, UserRoleModule, AuthModule, CompanyModule],
 })
 export class ApiModule {}
