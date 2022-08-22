e7c426b443fbc3a9addbf8a26daf6589a1df884d
Gabslima121 <glima@vixting.com.br>
2022-08-22 20:07:09 -0300

    added local postgre

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
-  const config: ConfigService = app.get(ConfigService);
-  const port: number = config.get<number>('PORT');
+  const port = process.env.PORT || 3200
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(port || 3200, () => {
-    console.log('[WEB]', config.get<string>('BASE_URL'));
+  await app.listen(port, () => {
+    console.log(`[WEB] Listenig on http://localhost:${port}`);
   });
 }
 
