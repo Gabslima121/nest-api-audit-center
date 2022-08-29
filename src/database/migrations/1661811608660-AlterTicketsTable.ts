b7da50cbdb4a26ce1851cfe05b2d9f9f7a519c4a
Gabslima121 <glima@vixting.com.br>
2022-08-29 20:33:49 -0300

    refactor some services

---
+++ src/database/migrations/1661811608660-AlterTicketsTable.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterTicketsTable1661811608660 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'tickets',
+      'open_date',
+      new TableColumn({
+        name: 'open_date',
+        type: 'date',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'tickets',
+      'limit_date',
+      new TableColumn({
+        name: 'limit_date',
+        type: 'date',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'tickets',
+      'close_date',
+      new TableColumn({
+        name: 'close_date',
+        type: 'date',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'tickets',
+      'open_date',
+      new TableColumn({
+        name: 'open_date',
+        type: 'varchar',
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'tickets',
+      'limit_date',
+      new TableColumn({
+        name: 'limit_date',
+        type: 'varchar',
+      }),
+    );
+
+    await queryRunner.changeColumn(
+      'tickets',
+      'close_date',
+      new TableColumn({
+        name: 'close_date',
+        type: 'varchar',
+      }),
+    );
+  }
+}
